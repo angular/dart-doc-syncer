d5fdc2e20ac744096ba84c19c908bbc064ff04a1
Thibault Sottiaux <thso@google.com>
2016-03-11 17:24:52 +0000

    Add logic to remove doc tag comments from the examples code.

---
+++ lib/src/remove_doc_tags.dart
@@ -0,0 +1,18 @@
+/// Returns [code] with filtered out doc tag comment lines.
+String removeDocTags(String code) =>
+    code.split('\n').where(_isNotDocTagComment).join('\n');
+
+final _docTags = ['#docregion', '#enddocregion', '#docplaster'];
+final _commentPrefixes = ['//', '<--'];
+
+/// Returns true if [line] is a not a comment line with a doc tag.
+bool _isNotDocTagComment(String line) => !_isDocTagComment(line);
+
+/// Returns true if [line] is a comment line with a doc tag.
+bool _isDocTagComment(String line) =>
+    _isComment(line) && _docTags.any((String tag) => line.contains(tag));
+
+/// Returns true if [line] starts with any of the prefixes indicating the start
+/// of a doc tag comment.
+bool _isComment(String line) =>
+    _commentPrefixes.any((String prefix) => line.trimLeft().startsWith(prefix));
