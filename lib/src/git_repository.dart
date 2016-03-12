cc936744ceba743ae013ee23b4d9ec5bebea2e09
Thibault Sottiaux <thso@google.com>
2016-03-12 17:20:18 +0000

    Clean-up git interaction, extracting into own file and allowing mocking for tests.

---
+++ lib/src/git_repository.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:logging/logging.dart';
+
+class GitRepository {
+  final _logger = new Logger('GitRepository');
+
+  final String directory;
+
+  GitRepository(this.directory);
+
+  /// Clones the git [repository] into this [directory].
+  Future cloneFrom(String repository) async {
+    _logger.fine('Cloning $repository into $directory.');
+    await _assertSuccess(
+        () => Process.run('git', ['clone', repository, directory]));
+  }
+
+  /// Deletes all files in this git [directory].
+  Future deleteAll() async {
+    _logger.fine('Deleting all repository content.');
+    await Process.run('git', ['rm', '*'], workingDirectory: directory);
+  }
+
+  /// Stages, commits with [message] and pushes local changes to origin for
+  /// [directory].
+  Future pushContent({String message}) async {
+    _logger.fine('Staging local changes for $directory.');
+    await _assertSuccess(
+        () => Process.run('git', ['add', '.'], workingDirectory: directory));
+
+    _logger.fine('Comitting changes for $directory.');
+    await _assertSuccess(() => Process.run('git', ['commit', '-m' '"$message"'],
+        workingDirectory: directory));
+
+    _logger.fine('Pushing changes for $directory.');
+    await _assertSuccess(
+        () => Process.run('git', ['push'], workingDirectory: directory));
+  }
+}
+
+/// Throws if the exitCode returned by [gitCommand] is not 0.
+Future _assertSuccess(Future gitCommand()) async {
+  final r = await gitCommand();
+  if (r.exitCode != 0) throw r.stderr;
+}
--- lib/update_doc_repo.dart
+++ lib/update_doc_repo.dart
@@ -3,28 +3,32 @@ import 'dart:io';
 
 import 'package:path/path.dart' as p;
 import 'package:logging/logging.dart';
+
 import 'package:dart_doc_syncer/src/generate_doc.dart';
+import 'package:dart_doc_syncer/src/git_helpers.dart';
 import 'package:dart_doc_syncer/src/remove_doc_tags.dart';
 
 final logger = new Logger('update_doc_repo');
 
 final String basePath = p.dirname(Platform.script.path);
-const String angularRepository = 'https://github.com/angular/angular.io';
+const String angularRepositoryUri = 'https://github.com/angular/angular.io';
 
 /// Updates [outRepository] based on the content of the example under
 /// [examplePath] in the angular.io repository.
-Future updateDocRepo(String examplePath, String outRepository) async {
+Future updateDocRepo(String examplePath, String outRepositoryUri) async {
   try {
     // Clone content of angular repo into tmp folder.
     final tmpAngularPath = p.join(basePath, '.tmp/angular_io');
-    await cloneInto(angularRepository, tmpAngularPath);
+    final angularRepository = new GitRepository(tmpAngularPath);
+    await angularRepository.cloneFrom(angularRepositoryUri);
 
     // Clone [outRepository] into tmp folder.
     final outPath = p.join(basePath, '.tmp/example_out');
-    await cloneInto(outRepository, outPath);
+    final outRepository = new GitRepository(outPath);
+    await outRepository.cloneFrom(outRepositoryUri);
 
     // Remove existing content as we will generate an updated version.
-    await Process.run('git', ['rm', '*'], workingDirectory: outPath);
+    await outRepository.deleteAll();
 
     logger.fine('Generating updated example application into $outPath.');
     final exampleFolder = p.join(tmpAngularPath, examplePath);
@@ -36,8 +40,8 @@ Future updateDocRepo(String examplePath, String outRepository) async {
     removeDocTagsFromApplication(exampleFolder);
 
     // Push the new content to [outRepository].
-    logger.fine('Pushing generated example to $outRepository');
-    await pushContent(outPath);
+    logger.fine('Pushing generated example to $outRepositoryUri');
+    await outRepository.pushContent(message: "Update...");
   } finally {
     // Clean up .tmp folder
     await new Directory(p.join(basePath, '.tmp')).delete(recursive: true);
@@ -57,34 +61,6 @@ Future removeDocTagsFromApplication(String path) async {
 
     await file.writeAsString(cleanedContent);
   }
-}
-
-/// Clones the [repository] into the [directory].
-Future cloneInto(String repository, String directory) async {
-  logger.fine('Cloning $repository into $directory');
-  await assertSuccess(
-      () => Process.run('git', ['clone', repository, directory]));
-}
-
-/// Stages, commits and pushes local changes to origin for [repositoryPath].
-pushContent(String repositoryPath) async {
-  logger.fine('Staging local changes for $repositoryPath');
-  await assertSuccess(
-      () => Process.run('git', ['add', '.'], workingDirectory: repositoryPath));
-
-  logger.fine('Comitting changes for $repositoryPath');
-  await assertSuccess(() => Process.run('git', ['commit', '-m' '"Update..."'],
-      workingDirectory: repositoryPath));
-
-  logger.fine('Pushing changes for $repositoryPath');
-  await assertSuccess(
-      () => Process.run('git', ['push'], workingDirectory: repositoryPath));
-}
-
-/// Throws if the exitCode returned by [gitCommand] is not 0.
-Future assertSuccess(Future gitCommand()) async {
-  final r = await gitCommand();
-  if (r.exitCode != 0) throw r.stderr;
 }
 
 main() async {
