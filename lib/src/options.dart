2474e841da3ab4907659da650701010b3d695a26
Patrice Chalin <pchalin@gmail.com>
2016-05-20 05:42:20 -0700

    Add support for: example name as single arg and --verbose
    - `dart_doc_syncer` now accepts one or two arguments. If one, it is
    taken as the example name from which the usual two argument path/URI
    are derived.
    - `dart_doc_syncer --verbose` will give details of what operations it
    is doing.
    - Tweak to generated README.md

--- lib/src/options.dart
+++ lib/src/options.dart
@@ -4,17 +4,19 @@ import 'package:args/args.dart';
 
 /// Global option
 bool dryRun = true;
-var _parser;
+bool verbose = false;
+ArgParser _parser;
 
 /// Processes command line options and returns remaining arguments.
 List<String> processArgs(List<String> args) {
   _parser = new ArgParser(allowTrailingOptions: true);
 
-  _parser.addFlag("help",
-      abbr: "h", negatable: false, help: "Shows usage information.");
-  const dryRunMsg = "Show which commands would be executed but make (almost) "
-      "no changes. (Only the temporary directory will be created and deleted.)";
-  _parser.addFlag("dry-run", abbr: "n", negatable: false, help: dryRunMsg);
+  _parser.addFlag('help',
+      abbr: 'h', negatable: false, help: 'Shows usage information.');
+  const dryRunMsg = 'Show which commands would be executed but make (almost) '
+      'no changes. (Only the temporary directory will be created and deleted.)';
+  _parser.addFlag('dry-run', abbr: 'n', negatable: false, help: dryRunMsg);
+  _parser.addFlag('verbose', abbr: 'v', negatable: false);
 
   var argResults;
   try {
@@ -23,20 +25,21 @@ List<String> processArgs(List<String> args) {
     printUsageAndExit(e.message, 0);
   }
 
-  if (argResults["help"]) printUsageAndExit(_parser);
+  if (argResults['help']) printUsageAndExit();
 
   dryRun = argResults['dry-run'];
+  verbose = argResults['verbose'];
   return argResults.rest;
 }
 
 void printUsageAndExit([String _msg, int exitCode = 1]) {
-  var msg = "Syncs angular.io example applications.";
+  var msg = 'Syncs angular.io example applications.';
   if (_msg != null) msg = _msg;
   print('''
 
 $msg.
 
-Usage: ${Platform.script} [options] <examplePath> <exampleRepository>
+Usage: ${Platform.script} [options] [<exampleName> | <examplePath> <exampleRepo>]
 
 ${_parser != null ? _parser.usage : ''}
 ''');
